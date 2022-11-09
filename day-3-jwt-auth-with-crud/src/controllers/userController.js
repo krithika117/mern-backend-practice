f81d0f84c1fa38cf77f6f630dda3890b99f580ee
krithika117 <krithikanithyanandam@gmail.com>
2022-11-09 12:50:44 +0530

    signin route

--- day-3-jwt-auth-with-crud/src/controllers/userController.js
+++ day-3-jwt-auth-with-crud/src/controllers/userController.js
@@ -21,12 +21,31 @@ const signup = async (req, res) => {
     }
     catch (err) {
         console.log(err);
-        res.status(500).json({message: "something went wrong"})
+        res.status(500).json({message: "Something went wrong"})
     }
 }
 
-const signin = (req, res) => {
+const signin = async (req, res) => {
+    const { email, password } = req.body;
 
+    try{
+        const existingUser = await userModel.findOne({ email:email })
+        if(!existingUser){
+            return res.status(404).json({message:"User not found"})
+        }
+        const matchPassword = await bcrypt.compare(password, existingUser.password)
+        if (!matchPassword){
+            return res.status(400).json({message:"Invalid Creds"})
+        }
+
+        const token = jwt.sign({email:existingUser.email, id: existingUser._id}, SECRET_KEY)
+        res.status(201).json({user: existingUser, token: token});
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Something went wrong"})
+    }
 }
+
 
 module.exports = { signup, signin }
